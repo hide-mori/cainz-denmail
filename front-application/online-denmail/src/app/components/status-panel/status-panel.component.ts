9558757bd1b3a398243c152a6db6c0a19a46fc6d
chengwanxin <chengwanxin@gmail.com>
2020-10-17 03:31:17 +0900

    種別カードの間隔、リストを5件に表示する。

--- front-application/online-denmail/src/app/components/status-panel/status-panel.component.ts
+++ front-application/online-denmail/src/app/components/status-panel/status-panel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, SimpleChanges, EventEmitter, Input, Output } from '@angular/core';
 
 import {StatusItem} from '../../modal/renrakuData'
+import {StatusUtility} from '../../utility/status-utility'
 
 @Component({
   selector: 'app-status-panel',
@@ -41,51 +42,12 @@ export class StatusPanelComponent implements OnInit, OnChanges {
   ngOnInit() {
           
       if(this.statusArr == null || this.statusArr.length==0) {
-     // 未読 作業中 作業者完了 作業者対象外 完了          
-     this.statusArr = [];
-     this.statusArr.push({
-       name: "未読",
-       name1:"",
-       statusId: '1',
-       varcolor: "--ion-color-status_unread",
-       cnt: 0,
-     });
-     this.statusArr.push({
-       name: "作業中",
-       name1:"",
-       statusId: '2',
-       varcolor: "--ion-color-status_working",
-       cnt: 0,
-     });
-     this.statusArr.push({
-       name: "作業者",
-       name1:"完了",
-       statusId: '3',
-       varcolor: "--ion-color-status_worker_completed",
-       cnt: 0,
-     });
-     this.statusArr.push({
-       name: "作業者",
-       name1: "対象外",
-       statusId: '4',
-       varcolor: "--ion-color-status_worker_excluded",
-       cnt: 0,
-     });
-     this.statusArr.push({
-       name: "対象外",
-       name1:"",
-       statusId: '5',
-       varcolor: "--ion-color-status_excluded",
-       cnt: 0,
-     });
-     this.statusArr.push({
-       name: "完了",
-       name1:"",
-       statusId: '6',
-       varcolor: "--ion-color-status_completed",
-       cnt: 0,
-     });
+        this.statusArr = StatusUtility.getStatusArr();
       }
-  }
 
+      /*
+      const el = document.querySelector('.status-card');
+      console.log(el);
+      */
+  }
 }
